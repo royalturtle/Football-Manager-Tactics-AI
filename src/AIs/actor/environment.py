75ad14a4e8eb02ea478bcdb60cf6daebf29365b4
royalturtle <aabut27@gmail.com>
2020-08-30 20:34:40 +0900

    - make action more flexible (repeat way)

---
+++ src/AIs/actor/environment.py
@@ -0,0 +1,16 @@
+from src.utils.TestPrinter import _TPI
+
+
+class Environment:
+    def __init__(self):
+        _TPI(self, locals())
+
+        def load_data_file():
+            pass
+
+    def get_players_stat(self, team):
+        _TPI(self, locals())
+        result = None
+        return result
+
+
